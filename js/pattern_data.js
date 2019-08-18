c0ece1c05ba7baeb54782e594cf9ed39177c880b
nomrand <42882840+nomrand@users.noreply.github.com>
2019-08-18 14:15:39 +0700

    delete images

--- js/pattern_data.js
+++ js/pattern_data.js
@@ -1,4 +1,10 @@
 let data = [
-    { img: "ar/pikachu.png", patt: "patterns/01.patt", },
-    { img: "ar/eevee.jpg", patt: "patterns/02.patt", },
+    { img: "ar/sn/01.png", patt: "patterns/sn/pattern-01.patt", },
+    { img: "ar/sn/02.png", patt: "patterns/sn/pattern-02.patt", },
+    { img: "ar/sn/03.png", patt: "patterns/sn/pattern-03.patt", },
+    { img: "ar/sn/04.png", patt: "patterns/sn/pattern-04.patt", },
+    { img: "ar/pk/01.png", patt: "patterns/pk/pattern-01.patt", },
+    { img: "ar/pk/02.png", patt: "patterns/pk/pattern-02.patt", },
+    { img: "ar/pk/03.png", patt: "patterns/pk/pattern-03.patt", },
+    { img: "ar/pk/04.png", patt: "patterns/pk/pattern-04.patt", },
 ];
