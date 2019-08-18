c82b70df75f463f152ec63173d3c058cc6bf0817
nomrand <42882840+nomrand@users.noreply.github.com>
2019-08-18 15:22:36 +0700

    to common

--- js/data.js
+++ js/data.js
@@ -1,5 +1,8 @@
 let data = [
-    "ar/pikachu.png",
-    "ar/eevee.jpg",
-    "ar/pug/pug.gltf",
+    { img: "ar/01.png", patt: "patterns/pattern-01.patt", },
+    { img: "ar/02.png", patt: "patterns/pattern-02.patt", },
+    { img: "ar/03.jpg", patt: "patterns/pattern-03.patt", },
+    { img: "ar/04.png", patt: "patterns/pattern-04.patt", },
+    { img: "ar/02.png", barcode: "1", },
+    { img: "ar/03.jpg", barcode: "2", rotate: "-90 0 0", },
 ];
